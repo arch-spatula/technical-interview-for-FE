6808e35d3ed37312492c62df504cfdc69554191e
arch-spatula <shkim516@naver.com>
2023-03-10 17:03:23 +0900

    순진한 문자열 탐색 테스트 통과

--- patterns/playground.test.ts
+++ patterns/playground.test.ts
@@ -5,4 +5,7 @@ describe("순진한 문자열 탐색을 구현합니다.", () => {
   test("wowomgzomg에서 omg 2개를 찾습니다.", () => {
     expect(solution("wowomgzomg", "omg")).toBe(2);
   });
+  test("harold said haha in hamburg에 ha 4개를 찾습니다.", () => {
+    expect(solution("harold said haha in hamburg", "ha")).toBe(4);
+  });
 });
--- patterns/playground.ts
+++ patterns/playground.ts
@@ -1,4 +1,17 @@
-export function solution(longString: string, target: string) {
+export function solution(longString: string, subString: string) {
   let count = 0;
-  return 2;
+  for (let i = 0; i < longString.length; i++) {
+    let subCount = 0;
+    if (longString[i] === subString[0]) {
+      for (let j = 0; j < subString.length; j++) {
+        if (longString[i + j] === subString[j]) {
+          subCount += 1;
+        } else {
+          break;
+        }
+        if (subCount === subString.length) count += 1;
+      }
+    }
+  }
+  return count;
 }
