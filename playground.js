97a2265bbf4966741c388fd0b29bf674aca533ef
arch-spatula <shkim516@naver.com>
2023-03-25 23:23:53 +0900

    실험

--- playground.js
+++ playground.js
@@ -1,40 +1,18 @@
-// hp	result
-// 23	5
-// 24	6
-// 999	201
-
-/**
- * @param {number} hp
- * @returns {number}
- */
-function solution(hp) {
-  const general = 5;
-  let generalCount = 0;
-  const soldier = 3;
-  let soldierCount = 0;
-  const worker = 1;
-  let workerCount = 0;
-
-  while (hp >= general) {
-    hp -= general;
-    generalCount += 1;
+function btoa(str) {
+  let base64 = "";
+  for (let i = 0; i < str.length; i += 3) {
+    let a = str.charCodeAt(i);
+    let b = str.charCodeAt(i + 1);
+    let c = str.charCodeAt(i + 2);
+    let triplet = (a << 16) | (b << 8) | c;
+    for (let j = 0; j < 4; j++) {
+      if (i * 8 + j * 6 <= str.length * 8) {
+        let index = (triplet >> (6 * (3 - j))) & 0x3f;
+        base64 += b64chars.charAt(index);
+      } else {
+        base64 += "=";
+      }
+    }
   }
-
-  while (hp >= soldier) {
-    hp -= soldier;
-    soldierCount += 1;
-  }
-
-  while (hp >= worker) {
-    hp -= worker;
-    workerCount += 1;
-  }
-
-  return generalCount + soldierCount + workerCount;
+  return base64;
 }
-
-console.log(solution(23), solution(23) === 5);
-
-console.log(solution(24), solution(24) === 6);
-
-console.log(solution(999), solution(999) === 201);
