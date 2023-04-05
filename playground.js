c7c08aaf1445467be7fea1513a62541f47d5ba32
arch-spatula <shkim516@naver.com>
2023-04-05 13:49:49 +0900

    vim 오류 임시 커밋

--- playground.js
+++ playground.js
@@ -1,5 +1,14 @@
-// const a = 1;
-// obj
+const arr = [1, 2, 3];
 
-const arr = [3];
-console.log(arr);
+/**
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+function addNum(arr, add = 0) {
+  arr.push(add);
+  return arr;
+}
+
+console.log(addNum(arr, 4)); // [1, 2, 3]
+console.log(addNum(arr, 5)); // [1, 2, 3]
+console.log(addNum(arr, 6)); // [1, 2, 3]
