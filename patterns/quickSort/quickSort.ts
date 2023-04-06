c3afbb3642698fe8da860357a0a044d258400bb8
arch-spatula <shkim516@naver.com>
2023-04-06 18:54:38 +0900

    퀵정렬 프로그램 수정

--- patterns/quickSort/quickSort.ts
+++ patterns/quickSort/quickSort.ts
@@ -5,13 +5,15 @@ export function swap(arr: number[], idx1: number, idx2: number) {
 export function pivot(arr: number[], start = 0, end = arr.length - 1) {
   const pivot = arr[start];
   let pivotIdx = start;
-  for (let i = 0; i < arr.length; i++) {
+
+  for (let i = start + 1; i < end + 1; i++) {
+    // 강의 예제랑 다르게 왜 end에 1을 더해야 정상동작하는가?
     if (pivot > arr[i]) {
-      pivotIdx++;
+      pivotIdx += 1;
       swap(arr, pivotIdx, i);
     }
   }
-  swap(arr, 0, pivotIdx);
+  swap(arr, start, pivotIdx);
   return pivotIdx;
 }
 
@@ -26,4 +28,4 @@ export function quickSort(arr: number[], left = 0, right = arr.length - 1) {
   return arr;
 }
 
-console.log(pivot([5, 3, 4, 1, 2]));
+// console.log(pivot([5, 3, 4, 1, 2]));
