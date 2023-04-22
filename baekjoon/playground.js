f23afc6c44d6ef08261fb6a5fd0143f17b1a72d7
arch-spatula <shkim516@naver.com>
2023-04-22 21:34:10 +0900

    코테 6문제 풀이

--- baekjoon/playground.js
+++ baekjoon/playground.js
@@ -1,29 +1,48 @@
 const fs = require("fs");
 const filePath =
   process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
-const [num] = fs
-  .readFileSync(filePath)
-  .toString()
-  .split("\n")
-  .map((str) => parseInt(str));
+const input = fs.readFileSync(filePath).toString().split("\n");
+const n = parseInt(input[0]);
+const str = input.slice(1, n + 1);
 
 /**
- * @param {number} num
- * @returns {string}
+ * @param {string} str
+ * @returns {boolean}
  */
-function solution(num) {
-  return Array.from({ length: 2 * num - 1 }, (_, idx) =>
-    Math.abs(num - idx - 1)
-  )
-    .map((elem) => num - elem)
-    .map((elem) => " ".repeat(num - elem) + "*".repeat(2 * elem - 1))
-    .join("\n");
+function isGroupWord(str) {
+  const memo = {};
+  let check = true;
+  str.split("").forEach((elem, idx, arr) => {
+    // 존재하고 직전과 다르면
+    if (memo[elem] && arr[idx - 1] !== elem) {
+      check = false;
+    }
+    // 존재하지 않으면 기록
+    if (!memo[elem]) {
+      memo[elem] = 1;
+    }
+  });
+  return check;
 }
 
-console.log(solution(num));
+/**
+ * @param {string} n
+ * @param {string[]} str
+ * @returns {number}
+ */
+function solution(n, str) {
+  const arr = str.slice(0, n).filter((elem) => {
+    // 이미 등장했던 글자가 다시 등장하면 false
+    return isGroupWord(elem);
+  });
+  return arr.length;
+}
+
+console.log(solution(n, str));
 
 module.exports = {
   solution,
+  isGroupWord,
 };
 
 // --------------------------------------------------------------------------
--- baekjoon/playground.test.js
+++ baekjoon/playground.test.js
@@ -1,26 +1,42 @@
-import { solution } from "./playground";
+import { isGroupWord, solution } from "./playground";
 import { test, expect, describe } from "vitest";
 
-describe("예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.", () => {
-  test("별", () => {
-    expect(solution(5)).toBe(`    *
-   ***
-  *****
- *******
-*********
- *******
-  *****
-   ***
-    *`);
+describe("단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램", () => {
+  test('"happy", "new", "year"', () => {
+    expect(solution(3, ["happy", "new", "year"])).toBe(3);
   });
 
-  test("1", () => {
-    expect(solution(1)).toBe(`*`);
+  test('"aba", "abab", "abcabc", "a"', () => {
+    expect(solution(4, ["aba", "abab", "abcabc", "a"])).toBe(1);
   });
 
-  test("2", () => {
-    expect(solution(2)).toBe(` *
-***
- *`);
+  test('"ab", "aa", "aca", "ba", "bb"', () => {
+    expect(solution(5, ["ab", "aa", "aca", "ba", "bb"])).toBe(4);
+  });
+
+  test('"yzyzy", "zyzyz"', () => {
+    expect(solution(2, ["yzyzy", "zyzyz"])).toBe(0);
+  });
+
+  test('"z"', () => {
+    expect(solution(1, ["z"])).toBe(1);
+  });
+
+  test('"a", "aba"', () => {
+    expect(solution(2, ["a", "aba"])).toBe(1);
+  });
+});
+
+describe("isGroupWord", () => {
+  test("true - a", () => {
+    expect(isGroupWord("a")).toBe(true);
+  });
+
+  test("false - aba", () => {
+    expect(isGroupWord("aba")).toBe(false);
+  });
+
+  test("true - aab", () => {
+    expect(isGroupWord("aab")).toBe(true);
   });
 });
