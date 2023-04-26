8edc82f024c52ebb1fe508ce778ab032963b3422
arch-spatula <shkim516@naver.com>
2023-04-26 14:50:11 +0900

    동적 프로그래밍 입문 정리

--- udemy/playground.js
+++ udemy/playground.js
@@ -1,7 +1,10 @@
-class WeightedGraph {
-  constructor() {
-    this.adjacencyList = {};
+function fib(n) {
+  if (n <= 2) return 1;
+  let fibNumbers = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2];
   }
+  return fibNumbers[n];
 }
 
-console.log(list.head);
+console.log(fib(10000)); // Infinity
