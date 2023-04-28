7ef5d0abebc610b8cc325760b548ce6896e33ea2
arch-spatula <shkim516@naver.com>
2023-04-28 16:04:58 +0900

    1일3제 기능개발 문제 풀이 추가

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,14 +1,53 @@
 /**
- * @param {number[]} arr
+ * @param {number[]} progresses
+ * @param {number[]} speed
  * @returns {number[]}
  */
-function solution(arr) {
+function solution(progresses, speed) {
   const queue = [];
-  arr.forEach((num, idx, arr) => {
-    const cache = arr[idx - 1];
-    if (num !== cache) queue.push(num);
+
+  /**
+   * @param {number} progresses
+   * @param {number} speed
+   * @returns {void}
+   */
+  const enqueue = (progresses, speed) => {
+    // 필요한 일 수
+    let days = 0;
+    while (progresses < 100) {
+      progresses += speed;
+      days += 1;
+    }
+    queue.push(days);
+  };
+
+  progresses.forEach((_, idx) => {
+    enqueue(progresses[idx], speed[idx]);
   });
-  return queue;
+
+  /**
+   * @param {number[]} queue
+   * @returns {number[]}
+   */
+  const dequeue = (queue) => {
+    const result = [];
+    let count = 0;
+    let day = queue[0];
+    for (let i = 0; i < queue.length; i++) {
+      // 까지
+      if (queue[i] > day) {
+        day = queue[i];
+        result.push(count);
+        count = 1;
+      } else {
+        count += 1;
+      }
+    }
+    result.push(count);
+    return result;
+  };
+
+  return dequeue(queue);
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -2,12 +2,36 @@ import solution from "./playground";
 import { test, expect, describe } from "vitest";
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/12906
-describe("같은 숫자는 싫어", () => {
+describe("배포마다 몇 개의 기능이 배포되는지를 return", () => {
   test("예제 1", () => {
-    expect(solution([1, 1, 3, 3, 0, 1, 1])).toEqual([1, 3, 0, 1]);
+    expect(solution([93, 30, 55], [1, 30, 5])).toEqual([2, 1]);
   });
 
   test("예제 2", () => {
-    expect(solution([4, 4, 4, 3, 3])).toEqual([4, 3]);
+    expect(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])).toEqual([
+      1, 3, 2,
+    ]);
+  });
+
+  // test("예제 3", () => {
+  //   expect(solution([90, 90, 90, 90], [30, 1, 1, 1])).toEqual([1, 3]);
+  // });
+
+  test("예제 4", () => {
+    expect(solution([95, 94], [3, 3])).toEqual([2]);
+  });
+
+  test("예제 5", () => {
+    expect(solution([99, 99, 99, 90, 90, 90], [1, 1, 1, 1, 1, 1])).toEqual([
+      3, 3,
+    ]);
+  });
+
+  test("예제 6", () => {
+    expect(solution([70], [20])).toEqual([1]);
+  });
+
+  test("예제 7", () => {
+    expect(solution([20, 20], [30, 5])).toEqual([1, 1]);
   });
 });
