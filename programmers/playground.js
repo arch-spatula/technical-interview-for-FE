0bab261b5c084612ceef265dd78a95a90515ad66
arch-spatula <shkim516@naver.com>
2023-05-18 14:15:20 +0900

    1일3제

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,15 +1,33 @@
 /**
- * @param {number} q
- * @param {number} r
- * @param {string} code
- * @returns {string}
+ * @param {string} s
+ * @returns {[number, number]}
  */
-function solution(q, r, code) {
-  let result = "";
-  code.split("").forEach((char, idx) => {
-    if (idx % q === r) result += char;
-  });
-  return result;
+function solution(s) {
+  let zeroCount = 0;
+  let processCount = 0;
+  let char = s;
+  while (char !== "1") {
+    const [binary, count] = process(char);
+    char = binary;
+    zeroCount += count;
+    processCount += 1;
+  }
+  return [processCount, zeroCount];
+}
+
+export function process(s) {
+  let zeroCount = 0;
+  return [
+    s
+      .split("")
+      .filter((char) => {
+        if (char === "0") zeroCount += 1;
+        return char === "1";
+      })
+      .join("")
+      .length.toString(2),
+    zeroCount,
+  ];
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,16 +1,33 @@
-import solution from "./playground";
+import solution, { process } from "./playground";
 import { test, expect, describe } from "vitest";
 
-// q	r	code	result
-// 3	1	"qjnwezgrpirldywt"	"jerry"
-// 1	0	"programmers"	"programmers"
+// s	result
+// "110010101001"	[3,8]
+// "01110"	[3,3]
+// "1111111"	[4,1]
 
-describe("최솟값 만들기", () => {
+describe("이진 변환 반복하기", () => {
   test("예제 1", () => {
-    expect(solution(3, 1, "qjnwezgrpirldywt")).toBe("jerry");
+    expect(solution("110010101001")).toEqual([3, 8]);
   });
 
   test("예제 2", () => {
-    expect(solution(1, 0, "programmers")).toBe("programmers");
+    expect(solution("01110")).toEqual([3, 3]);
+  });
+
+  test("예제 3", () => {
+    expect(solution("1111111")).toEqual([4, 1]);
+  });
+
+  test("process 1", () => {
+    expect(process("110010101001")).toEqual(["110", 6]);
+  });
+
+  test("process 2", () => {
+    expect(process("110")).toEqual(["10", 1]);
+  });
+
+  test("process 3", () => {
+    expect(process("10")).toEqual(["1", 1]);
   });
 });
