bd8cf12f6770ab179768e669353378126fdec63f
arch-spatula <shkim516@naver.com>
2023-05-19 13:42:11 +0900

    1일 3제

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,33 +1,29 @@
 /**
- * @param {string} s
- * @returns {[number, number]}
+ * @param {">" | "<"} ineq
+ * @param {"!" | "="} eq
+ * @param {number} n
+ * @param {number} m
+ * @returns {1 | 0}
  */
-function solution(s) {
-  let zeroCount = 0;
-  let processCount = 0;
-  let char = s;
-  while (char !== "1") {
-    const [binary, count] = process(char);
-    char = binary;
-    zeroCount += count;
-    processCount += 1;
+function solution(ineq, eq, n, m) {
+  let result = `${n}`;
+  switch (ineq) {
+    case "<":
+      result += "<";
+      break;
+    case ">":
+      result += ">";
   }
-  return [processCount, zeroCount];
-}
+  switch (eq) {
+    case "!":
+      result += "";
+      break;
+    case "=":
+      result += "=";
+  }
+  result += `${m}`;
 
-export function process(s) {
-  let zeroCount = 0;
-  return [
-    s
-      .split("")
-      .filter((char) => {
-        if (char === "0") zeroCount += 1;
-        return char === "1";
-      })
-      .join("")
-      .length.toString(2),
-    zeroCount,
-  ];
+  return eval(result) ? 1 : 0;
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,33 +1,12 @@
-import solution, { process } from "./playground";
+import solution from "./playground";
 import { test, expect, describe } from "vitest";
 
-// s	result
-// "110010101001"	[3,8]
-// "01110"	[3,3]
-// "1111111"	[4,1]
-
-describe("이진 변환 반복하기", () => {
+describe("외계어 사전", () => {
   test("예제 1", () => {
-    expect(solution("110010101001")).toEqual([3, 8]);
+    expect(solution("<", "=", 20, 50, 1)).toBe(1);
   });
 
   test("예제 2", () => {
-    expect(solution("01110")).toEqual([3, 3]);
-  });
-
-  test("예제 3", () => {
-    expect(solution("1111111")).toEqual([4, 1]);
-  });
-
-  test("process 1", () => {
-    expect(process("110010101001")).toEqual(["110", 6]);
-  });
-
-  test("process 2", () => {
-    expect(process("110")).toEqual(["10", 1]);
-  });
-
-  test("process 3", () => {
-    expect(process("10")).toEqual(["1", 1]);
+    expect(solution(">", "!", 41, 78, 0)).toBe(0);
   });
 });
