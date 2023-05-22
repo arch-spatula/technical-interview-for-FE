9efd17b8b283790e2f7a50a39d9e0ad721aca2e7
arch-spatula <shkim516@naver.com>
2023-05-22 12:27:23 +0900

    1일3제

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,28 +1,10 @@
 /**
- * @param {number[]} arr
- * @returns {number[]}
+ * @param {number} M
+ * @param {number} N
+ * @returns {number}
  */
-function solution(arr) {
-  const result = [];
-  let twoFlag = false;
-  const twoStack = [];
-
-  let cacheTwo = [];
-  arr.forEach((num) => {
-    cacheTwo.push(num);
-    if (num === 2) {
-      twoStack.push(...cacheTwo);
-      cacheTwo = [];
-    }
-  });
-  twoStack.forEach((num) => {
-    if (num === 2) twoFlag = true;
-    if (twoFlag) result.push(num);
-  });
-
-  if (result.length === 0) result.push(-1);
-
-  return result;
+function solution(M, N) {
+  return M * N - 1;
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -3,15 +3,21 @@ import { test, expect, describe } from "vitest";
 
 describe("2의 영역", () => {
   test("예제 1", () => {
-    expect(solution([1, 2, 1, 4, 5, 2, 9])).toEqual([2, 1, 4, 5, 2]);
+    expect(solution(2, 2)).toBe(3);
   });
   test("예제 2", () => {
-    expect(solution([1, 2, 1])).toEqual([2]);
+    expect(solution(2, 5)).toBe(9);
   });
   test("예제 3", () => {
-    expect(solution([1, 1, 1])).toEqual([-1]);
+    expect(solution(1, 1)).toBe(0);
   });
   test("예제 4", () => {
-    expect(solution([1, 2, 1, 2, 1, 10, 2, 1])).toEqual([2, 1, 2, 1, 10, 2]);
+    expect(solution(2, 1)).toBe(1);
+  });
+  test("예제 5", () => {
+    expect(solution(1, 2)).toBe(1);
+  });
+  test("예제 6", () => {
+    expect(solution(4, 4)).toBe(15);
   });
 });
