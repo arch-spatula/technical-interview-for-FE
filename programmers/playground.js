a4ffc376f8dbe3799079449bb4b252abac9afa7d
arch-spatula <shkim516@naver.com>
2023-05-23 14:10:52 +0900

    1일3제

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,10 +1,11 @@
 /**
- * @param {number} M
- * @param {number} N
+ * @param {number} n
  * @returns {number}
  */
-function solution(M, N) {
-  return M * N - 1;
+function solution(n) {
+  return Array.from({ length: n }, (_, idx) => idx + 1).filter(
+    (num) => n % num === 0 && num % 2 !== 0
+  ).length;
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,23 +1,8 @@
 import solution from "./playground";
 import { test, expect, describe } from "vitest";
 
-describe("2의 영역", () => {
+describe("숫자의 표현", () => {
   test("예제 1", () => {
-    expect(solution(2, 2)).toBe(3);
-  });
-  test("예제 2", () => {
-    expect(solution(2, 5)).toBe(9);
-  });
-  test("예제 3", () => {
-    expect(solution(1, 1)).toBe(0);
-  });
-  test("예제 4", () => {
-    expect(solution(2, 1)).toBe(1);
-  });
-  test("예제 5", () => {
-    expect(solution(1, 2)).toBe(1);
-  });
-  test("예제 6", () => {
-    expect(solution(4, 4)).toBe(15);
+    expect(solution(15)).toBe(4);
   });
 });
