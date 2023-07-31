a04105a1cfd3cbcc67dface5287922e793121a52
arch-spatula <shkim516@naver.com>
2023-07-31 14:24:12 +0900

    1일3제 달성

--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,19 +1,14 @@
 import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// a	b	  result
-// 7	20	1
-// 11	22	1
-// 12	21	2
-
-describe('유한소수 판별하기', () => {
+describe('시저 암호', () => {
   test('예제 1', () => {
-    expect(solution(7, 20)).toBe(1);
+    expect(solution('AB', 1)).toBe('BC');
   });
   test('예제 2', () => {
-    expect(solution(11, 22)).toBe(1);
+    expect(solution('z', 1)).toBe('a');
   });
   test('예제 3', () => {
-    expect(solution(12, 21)).toBe(2);
+    expect(solution('a B z', 4)).toBe('e F d');
   });
 });
