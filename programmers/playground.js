835516696b749864bd97b6fe78004435795e0dbd
arch-spatula <shkim516@naver.com>
2023-08-10 14:21:18 +0900

    1일3제: 영어 끝말잇기 풀이

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,38 +1,38 @@
 /**
- * @param {number} brown
- * @param {number} yellow
+ * @param {number} n
+ * @param {string[]} words
  * @returns {[number, number]}
  */
-function solution(brown, yellow) {
-  // 노랑의 약수 배열을 구함
-  const divisors = Array.from({ length: yellow }, (_, idx) => idx + 1).filter(
-    (num) => yellow % num === 0
-  );
+function solution(n, words) {
+  const wordMemo = new Map();
+  const playerTurnMemo = new Map();
+  let latestWord = '';
 
-  // 양끝 투포인터 준비
-  if (divisors.length % 2 !== 0) {
-    let sqrtVal = 0;
-    let sqrtIdx = 0;
-    // 동일한 제곱값 하나더 추가
-    divisors.forEach((num, idx) => {
-      if (num ** 2 === yellow) {
-        sqrtVal = num;
-        sqrtIdx = idx;
-      }
-    });
-    divisors.splice(sqrtIdx, 0, sqrtVal);
+  for (let i = 0; i < n; i++) {
+    playerTurnMemo.set(i + 1, 0);
   }
 
-  // 노랑의 가로 >= 노랑의 세로 동안(while)
-  for (let i = 0; i < divisors.length / 2; i++) {
-    const width = divisors[divisors.length - i - 1];
-    const hight = divisors[i];
-    // 박스배열의 변 + 꼭지점 개수 갈색과 비교
-    if ((width + hight) * 2 + 4 === brown) {
-      // 일치하면 갈색 포함해서 가로 새로값 정답
-      return [width + 2, hight + 2];
+  for (let i = 0; i < words.length; i++) {
+    const currentPlayer = (i % n) + 1;
+    const word = words[i];
+    playerTurnMemo.set(currentPlayer, playerTurnMemo.get(currentPlayer) + 1);
+
+    if (i === 0) {
+      latestWord = word;
+      wordMemo.set(word, 1);
+      continue;
     }
+
+    if (latestWord.at(-1) !== words[i][0])
+      return [currentPlayer, playerTurnMemo.get(currentPlayer)];
+    if (wordMemo.get(word))
+      return [currentPlayer, playerTurnMemo.get(currentPlayer)];
+
+    wordMemo.set(word, 1);
+    latestWord = word;
   }
+
+  return [0, 0];
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,19 +1,51 @@
 import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// brown	yellow	return
-// 10	    2	      [4, 3]
-// 8	    1	      [3, 3]
-// 24	    24	    [8, 6]
+// n	words	result
+// 3	["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
+// 5	["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]	[0,0]
+// 2	["hello", "one", "even", "never", "now", "world", "draw"]	[1,3]
 
 describe('카펫', () => {
   test('예제 1', () => {
-    expect(solution(10, 2)).toEqual([4, 3]);
+    expect(
+      solution(3, [
+        'tank',
+        'kick',
+        'know',
+        'wheel',
+        'land',
+        'dream',
+        'mother',
+        'robot',
+        'tank',
+      ])
+    ).toEqual([3, 3]);
   });
   test('예제 2', () => {
-    expect(solution(8, 1)).toEqual([3, 3]);
+    expect(
+      solution(5, [
+        'hello',
+        'observe',
+        'effect',
+        'take',
+        'either',
+        'recognize',
+        'encourage',
+        'ensure',
+        'establish',
+        'hang',
+        'gather',
+        'refer',
+        'reference',
+        'estimate',
+        'executive',
+      ])
+    ).toEqual([0, 0]);
   });
   test('예제 3', () => {
-    expect(solution(24, 24)).toEqual([8, 6]);
+    expect(
+      solution(2, ['hello', 'one', 'even', 'never', 'now', 'world', 'draw'])
+    ).toEqual([1, 3]);
   });
 });
