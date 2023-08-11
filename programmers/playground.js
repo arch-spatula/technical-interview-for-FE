6bc126d4d241736194db34dddf97d13a15ebda73
arch-spatula <shkim516@naver.com>
2023-08-11 14:38:51 +0900

    1일 3제: 다항식 더하기, K번째수 풀이

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,38 +1,16 @@
 /**
- * @param {number} n
- * @param {string[]} words
- * @returns {[number, number]}
+ * @param {number[]} array,
+ * @param {[number, number, number][]} commands
+ * @returns {number[]}
  */
-function solution(n, words) {
-  const wordMemo = new Map();
-  const playerTurnMemo = new Map();
-  let latestWord = '';
+function solution(array, commands) {
+  const result = [];
+  commands.forEach((elem) => {
+    const [i, j, k] = elem;
 
-  for (let i = 0; i < n; i++) {
-    playerTurnMemo.set(i + 1, 0);
-  }
-
-  for (let i = 0; i < words.length; i++) {
-    const currentPlayer = (i % n) + 1;
-    const word = words[i];
-    playerTurnMemo.set(currentPlayer, playerTurnMemo.get(currentPlayer) + 1);
-
-    if (i === 0) {
-      latestWord = word;
-      wordMemo.set(word, 1);
-      continue;
-    }
-
-    if (latestWord.at(-1) !== words[i][0])
-      return [currentPlayer, playerTurnMemo.get(currentPlayer)];
-    if (wordMemo.get(word))
-      return [currentPlayer, playerTurnMemo.get(currentPlayer)];
-
-    wordMemo.set(word, 1);
-    latestWord = word;
-  }
-
-  return [0, 0];
+    result.push(array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+  });
+  return result;
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,51 +1,20 @@
 import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// n	words	result
-// 3	["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]	[3,3]
-// 5	["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]	[0,0]
-// 2	["hello", "one", "even", "never", "now", "world", "draw"]	[1,3]
+// array	                commands	                        return
+// [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
 
-describe('카펫', () => {
+describe('K번째수', () => {
   test('예제 1', () => {
     expect(
-      solution(3, [
-        'tank',
-        'kick',
-        'know',
-        'wheel',
-        'land',
-        'dream',
-        'mother',
-        'robot',
-        'tank',
-      ])
-    ).toEqual([3, 3]);
-  });
-  test('예제 2', () => {
-    expect(
-      solution(5, [
-        'hello',
-        'observe',
-        'effect',
-        'take',
-        'either',
-        'recognize',
-        'encourage',
-        'ensure',
-        'establish',
-        'hang',
-        'gather',
-        'refer',
-        'reference',
-        'estimate',
-        'executive',
-      ])
-    ).toEqual([0, 0]);
-  });
-  test('예제 3', () => {
-    expect(
-      solution(2, ['hello', 'one', 'even', 'never', 'now', 'world', 'draw'])
-    ).toEqual([1, 3]);
+      solution(
+        [1, 5, 2, 6, 3, 7, 4],
+        [
+          [2, 5, 3],
+          [4, 4, 1],
+          [1, 7, 3],
+        ]
+      )
+    ).toEqual([5, 6, 3]);
   });
 });
