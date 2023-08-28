6b5475f3e8fb738102e009dd8253020f18a4c7c3
arch-spatula <shkim516@naver.com>
2023-08-28 21:37:29 +0900

    1일3제: N개의 최소공배수

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,24 +1,61 @@
-// 이름       이동 거리             건전지 사용량
-// 순간이동    (현재까지 온 거리) x 2  0
-// 점프       K                   K
-
-/**
- * @param {number} n
- * @returns {number}
- */
-function solution(n) {
-  let jumpCount = 1;
-  let backTrack = n;
-
-  while (1 < backTrack) {
-    if (backTrack % 2 === 0) backTrack = backTrack / 2;
-    else {
-      jumpCount += 1;
-      backTrack -= 1;
+// 최소공배수
+export function lcm(x, y) {
+  const result = [];
+  let dev = 2;
+  let n = x;
+  let m = y;
+  while (dev <= n && dev <= m) {
+    if (n % dev === 0 && m % dev === 0) {
+      n = parseInt(n / dev);
+      m = parseInt(m / dev);
+      result.push(dev);
+      dev = 2;
+    } else {
+      dev += 1;
     }
   }
+  result.push(n);
+  result.push(m);
+  return result.reduce((acc, curr) => acc * curr);
+}
 
-  return jumpCount;
+/**
+ * @param {number[]} arr
+ * @returns {number}
+ */
+// function solution(arr) {
+//   const multiple = [];
+//   let copyArr = arr.slice();
+//   let div = 2;
+
+//   while (div <= Math.max(...copyArr)) {
+//     if (copyArr.every((num) => num % div === 0)) {
+//       copyArr = copyArr.map((num) => parseInt(num / div));
+//       multiple.push(div);
+//       div = 2;
+//     } else {
+//       div += 1;
+//     }
+//   }
+
+//   return copyArr.concat(multiple).reduce((acc, curr) => acc * curr);
+// }
+
+function solution(arr) {
+  arr.sort((a, b) => b - a);
+  let result,
+    isDivide = false,
+    count = arr[0];
+
+  while (!isDivide) {
+    isDivide = arr.every((num) => count % num === 0);
+    if (isDivide) {
+      result = count;
+      break;
+    }
+    count += 1;
+  }
+  return result;
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,43 +1,27 @@
 import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// N	  result
-// 5	  2
-// 6	  2
-// 5000	5
+// arr	      result
+// [2,6,8,14]	168
+// [1,2,3]	  6
 
-describe('점프와 순간 이동', () => {
+describe('N개의 최소공배수', () => {
   test('예제 1', () => {
-    expect(solution(5)).toBe(2);
-    // 0 -> 1 J 1
-    // 1 -> 2 T 1
-    // 2 -> 4 T 1
-    // 4 -> 5 J 2
+    expect(solution([2, 6, 8, 14])).toBe(168);
   });
   test('예제 2', () => {
-    expect(solution(6)).toBe(2);
-    // 0 -> 1 J 1
-    // 1 -> 2 T 1
-    // 2 -> 3 J 2
-    // 3 -> 6 T 2
+    expect(solution([1, 2, 3])).toBe(6);
   });
   test('예제 3', () => {
-    expect(solution(5000)).toBe(5);
-    //    0 ->    1 J 1
-    //    1 ->    2 T 1
-    //    2 ->    4 T 1
-    //    4 ->    8 T 1
-    //    8 ->    9 J 2
-    //    9 ->   18 T 2
-    //   18 ->   19 J 3
-    //   19 ->   38 T 3
-    //   38 ->   39 J 4
-    //   39 ->   78 T 4
-    //   78 ->  156 T 4
-    //  156 ->  312 T 4
-    //  624 ->  625 J 5
-    //  625 -> 1250 T 5
-    // 1250 -> 2500 T 5
-    // 2500 -> 5000 T 5
+    expect(solution([2, 7])).toBe(14);
+  });
+  test('예제 4', () => {
+    expect(solution([12, 32, 45, 67, 72])).toBe(96480);
+  });
+  test('예제 5', () => {
+    expect(solution([17, 34])).toBe(34);
+  });
+  test('예제 6', () => {
+    expect(solution([4, 4])).toBe(4);
   });
 });
