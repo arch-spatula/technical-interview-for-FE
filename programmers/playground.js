2e755f6470a25415e7b3996a271e8534fc06c104
arch-spatula <shkim516@naver.com>
2023-09-01 15:44:25 +0900

    1일3제: 최빈값 구하기

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,18 +1,18 @@
 /**
- * @param {number} n
- * @returns {number}
+ * @param {string[]} name
+ * @param {number[]} yearning
+ * @param {string[][]} photo
+ * @returns {number[]}
  */
-function solution(n) {
-  const MOD = 1234567;
-  const dp = new Array(n + 1).fill(0);
-  dp[1] = 1;
-  dp[2] = 2;
+function solution(name, yearning, photo) {
+  const memo = new Map();
+  name.forEach((name, idx) => {
+    memo.set(name, yearning[idx]);
+  });
 
-  for (let i = 3; i <= n; i++) {
-    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-  }
-
-  return dp[n];
+  return photo.map((elem) => {
+    return elem.reduce((acc, curr) => acc + (memo.get(curr) ?? 0), 0);
+  });
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,24 +1,45 @@
 import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// n	result
-// 4	5
-// 3	3
+// name	                            yearning	    photo	                                                                                            result
+// ["may", "kein", "kain", "radi"]	[5, 10, 1, 3]	[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]	[19, 15, 6]
+// ["kali", "mari", "don"]	        [11, 1, 55]	  [["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]	                      [67, 0, 55]
+// ["may", "kein", "kain", "radi"]	[5, 10, 1, 3]	[["may"],["kein", "deny", "may"], ["kon", "coni"]]	                                              [5, 15, 0]
 
 describe('멀리 뛰기', () => {
   test('예제 1', () => {
-    expect(solution(4)).toBe(5);
+    expect(
+      solution(
+        ['may', 'kein', 'kain', 'radi'],
+        [5, 10, 1, 3],
+        [
+          ['may', 'kein', 'kain', 'radi'],
+          ['may', 'kein', 'brin', 'deny'],
+          ['kon', 'kain', 'may', 'coni'],
+        ]
+      )
+    ).toEqual([19, 15, 6]);
   });
   test('예제 2', () => {
-    expect(solution(3)).toBe(3);
+    expect(
+      solution(
+        ['kali', 'mari', 'don'],
+        [11, 1, 55],
+        [
+          ['kali', 'mari', 'don'],
+          ['pony', 'tom', 'teddy'],
+          ['con', 'mona', 'don'],
+        ]
+      )
+    ).toEqual([67, 0, 55]);
   });
   test('예제 3', () => {
-    expect(solution(1)).toBe(1);
-  });
-  test('예제 4', () => {
-    expect(solution(2)).toBe(2);
-  });
-  test('예제 5', () => {
-    expect(solution(2000)).toBe(694725);
+    expect(
+      solution(
+        ['may', 'kein', 'kain', 'radi'],
+        [5, 10, 1, 3],
+        [['may'], ['kein', 'deny', 'may'], ['kon', 'coni']]
+      )
+    ).toEqual([5, 15, 0]);
   });
 });
