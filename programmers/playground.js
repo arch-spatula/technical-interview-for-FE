3817cdf1a7cac34e58c6f7456fa62aff1715d00a
arch-spatula <shkim516@naver.com>
2023-09-02 16:50:38 +0900

    1일3제: 명예의 전당, 귤 고르기 추가

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,18 +1,46 @@
 /**
- * @param {string[]} name
- * @param {number[]} yearning
- * @param {string[][]} photo
+ * @param {number} k
+ * @param {number[]} score
+ * @returns {number}
+ */
+function solution(k, score) {
+  let survivors = [];
+  const result = [];
+
+  for (let scoreIdx = 0; scoreIdx < score.length; scoreIdx++) {
+    survivors = cutOff(insert(score[scoreIdx], survivors), k);
+    result.push(survivors.at(-1));
+  }
+  return result;
+}
+
+/**
+ * @param {number} num
+ * @param {number[]} arr
  * @returns {number[]}
  */
-function solution(name, yearning, photo) {
-  const memo = new Map();
-  name.forEach((name, idx) => {
-    memo.set(name, yearning[idx]);
-  });
+export function insert(num, arr) {
+  const result = arr.slice();
+  let insertFlag = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (num >= arr[i]) {
+      result.splice(i, 0, num);
+      insertFlag = true;
+      break;
+    }
+  }
+  if (!insertFlag) result.push(num);
 
-  return photo.map((elem) => {
-    return elem.reduce((acc, curr) => acc + (memo.get(curr) ?? 0), 0);
-  });
+  return result;
+}
+
+/**
+ * @param {number[]} arr
+ * @param {number} maxLength
+ * @returns {number[]}
+ */
+export function cutOff(arr, maxLength) {
+  return arr.slice(0, maxLength);
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,45 +1,57 @@
-import solution from './playground';
+import solution, { cutOff, insert } from './playground';
 import { test, expect, describe } from 'vitest';
 
-// name	                            yearning	    photo	                                                                                            result
-// ["may", "kein", "kain", "radi"]	[5, 10, 1, 3]	[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]	[19, 15, 6]
-// ["kali", "mari", "don"]	        [11, 1, 55]	  [["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]	                      [67, 0, 55]
-// ["may", "kein", "kain", "radi"]	[5, 10, 1, 3]	[["may"],["kein", "deny", "may"], ["kon", "coni"]]	                                              [5, 15, 0]
+// k	score	                                        result
+// 3	[10, 100, 20, 150, 1, 100, 200]	              [10, 10, 10, 20, 20, 100, 100]
+// 4	[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
 
-describe('멀리 뛰기', () => {
+describe('명예의 전당 (1)', () => {
   test('예제 1', () => {
-    expect(
-      solution(
-        ['may', 'kein', 'kain', 'radi'],
-        [5, 10, 1, 3],
-        [
-          ['may', 'kein', 'kain', 'radi'],
-          ['may', 'kein', 'brin', 'deny'],
-          ['kon', 'kain', 'may', 'coni'],
-        ]
-      )
-    ).toEqual([19, 15, 6]);
+    expect(solution(3, [10, 100, 20, 150, 1, 100, 200])).toEqual([
+      10, 10, 10, 20, 20, 100, 100,
+    ]);
   });
   test('예제 2', () => {
-    expect(
-      solution(
-        ['kali', 'mari', 'don'],
-        [11, 1, 55],
-        [
-          ['kali', 'mari', 'don'],
-          ['pony', 'tom', 'teddy'],
-          ['con', 'mona', 'don'],
-        ]
-      )
-    ).toEqual([67, 0, 55]);
+    expect(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000])).toEqual([
+      0, 0, 0, 0, 20, 40, 70, 70, 150, 300,
+    ]);
   });
   test('예제 3', () => {
-    expect(
-      solution(
-        ['may', 'kein', 'kain', 'radi'],
-        [5, 10, 1, 3],
-        [['may'], ['kein', 'deny', 'may'], ['kon', 'coni']]
-      )
-    ).toEqual([5, 15, 0]);
+    expect(solution(3, [10, 20, 30, 40, 50])).toEqual([10, 10, 10, 20, 30]);
+  });
+  test('예제 4', () => {
+    expect(solution(5, [100, 200, 300, 400, 500])).toEqual([
+      100, 100, 100, 100, 100,
+    ]);
+  });
+  test('예제 5', () => {
+    expect(solution(3, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])).toEqual([
+      10, 9, 8, 8, 8, 8, 8, 8, 8, 8,
+    ]);
+    // [10] -> [10, 9] -> [10, 9, 8] -> ...
+  });
+  test('예제 6', () => {
+    expect(solution(2, [5, 5, 5, 5, 5, 5])).toEqual([5, 5, 5, 5, 5, 5]);
+  });
+});
+
+describe('insert', () => {
+  test('없으면 추가', () => {
+    expect(insert(1, [])).toEqual([1]);
+  });
+  test('앞에 추가', () => {
+    expect(insert(3, [2, 1])).toEqual([3, 2, 1]);
+  });
+  test('뒤에 추가', () => {
+    expect(insert(8, [10, 9])).toEqual([10, 9, 8]);
+  });
+  test('중간에 추가', () => {
+    expect(insert(9, [10, 8])).toEqual([10, 9, 8]);
+  });
+});
+
+describe('cutOff', () => {
+  test('cutOff', () => {
+    expect(cutOff([1, 2, 3, 4], 3)).toEqual([1, 2, 3]);
   });
 });
