b3d32e8ad3f76ffc6c458ceadf28278b7a1f7512
arch-spatula <shkim516@naver.com>
2023-09-07 17:47:21 +0900

    1일 3제: n^2 배열 자르기, 2016년

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,45 +1,14 @@
 /**
- * @typedef {"["|"]"|"{"|"}"|"("|")"} BracketType
- * @param {`${BracketType}`} s
- * @returns {number}
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @returns {number[]}
  */
-function solution(s) {
-  let result = 0;
-  for (let i = 0; i < s.length; i++) {
-    const brackets = [...s.split(''), ...s.split('')];
-    if (isCorrect(brackets.slice(i, i + s.length))) result += 1;
-  }
-  return result;
-}
-
-/**
- * @param {("["|"]"|"{"|"}"|"("|")")[]} s
- * @returns {boolean}
- */
-export function isCorrect(s) {
-  const stack = [];
-  for (let i = 0; i < s.length; i++) {
-    stack.push(s[i]);
-    if (
-      isPare(stack, '(', ')') ||
-      isPare(stack, '[', ']') ||
-      isPare(stack, '{', '}')
-    ) {
-      stack.pop();
-      stack.pop();
-    }
-  }
-  return stack.length === 0;
-}
-
-/**
- * @param {("["|"]"|"{"|"}"|"("|")")[]} s
- * @param {("["|"]"|"{"|"}"|"("|")")} left
- * @param {("["|"]"|"{"|"}"|"("|")")} right
- * @returns {boolean}
- */
-function isPare(arr, left, right) {
-  return arr.at(-2) === left && arr.at(-1) === right;
+function solution(n, left, right) {
+  return Array.from({ length: right - left + 1 }, (_, idx) => {
+    const [i, j] = [Math.floor((idx + left) / n) + 1, ((idx + left) % n) + 1];
+    return Math.max(i, j);
+  });
 }
 
 export default solution;
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,38 +1,15 @@
-import solution, { isCorrect, sumWindow } from './playground';
+import solution from './playground';
 import { test, expect, describe } from 'vitest';
 
-// s	      result
-// "[](){}"	3
-// "}]()[{"	2
-// "[)(]"	  0
-// "}}}"	  0
+// n	left	right	result
+// 3	2	    5	    [3,2,2,3]
+// 4	7	    14	  [4,3,3,3,4,4,4,4]
 
-describe('연속 부분 수열 합의 개수', () => {
+describe('n^2 배열 자르기', () => {
   test('예제 1', () => {
-    expect(solution('[](){}')).toBe(3);
+    expect(solution(3, 2, 5)).toEqual([3, 2, 2, 3]);
   });
   test('예제 2', () => {
-    expect(solution('}]()[{')).toBe(2);
-  });
-  test('예제 3', () => {
-    expect(solution('[)(]')).toBe(0);
-  });
-  test('예제 4', () => {
-    expect(solution('}}}')).toBe(0);
-  });
-});
-
-describe('isCorrect', () => {
-  test('예제 1', () => {
-    expect(isCorrect('[](){}')).toBe(true);
-  });
-  test('예제 2', () => {
-    expect(isCorrect('](){}[')).toBe(false);
-  });
-  test('예제 3', () => {
-    expect(isCorrect('[{}]()')).toBe(true);
-  });
-  test('예제 4', () => {
-    expect(isCorrect('}')).toBe(false);
+    expect(solution(4, 7, 14)).toEqual([4, 3, 3, 3, 4, 4, 4, 4]);
   });
 });
