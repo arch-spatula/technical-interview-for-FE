157606b6e33b5b1b9395d178fe77c36228ea8e3f
arch-spatula <shkim516@naver.com>
2023-09-12 12:50:41 +0900

    1일3제: 할인 행사, 카드 뭉치

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,14 +1,33 @@
 /**
- * @param {number} n
- * @param {number} left
- * @param {number} right
- * @returns {number[]}
+ * @param {string[]} want
+ * @param {number[]} number
+ * @param {string[]} discount
+ * @returns {number}
  */
-function solution(n, left, right) {
-  return Array.from({ length: right - left + 1 }, (_, idx) => {
-    const [i, j] = [Math.floor((idx + left) / n) + 1, ((idx + left) % n) + 1];
-    return Math.max(i, j);
-  });
+function solution(want, number, discount) {
+  let result = 0;
+  let flag = false;
+  for (let i = 0; i <= discount.length - 10; i++) {
+    const groceryList = new Map();
+    want.forEach((elem, idx) => {
+      groceryList.set(elem, number[idx]);
+    });
+
+    const window = discount.slice(i, i + 10);
+    for (let j = 0; j < window.length; j++) {
+      if (groceryList.get(window[j]) !== undefined) {
+        groceryList.set(window[j], groceryList.get(window[j]) - 1, i);
+      }
+    }
+
+    if (
+      new Set(groceryList.values()).size === 1 &&
+      new Set(groceryList.values()).has(0)
+    ) {
+      result += 1;
+    }
+  }
+  return result;
 }
 
 export default solution;
