91c5c239ea0dcc4894acb5f3d124883fb9f746c2
arch-spatula <shkim516@naver.com>
2023-09-14 18:08:37 +0900

    1일3제: 1차 캐시

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,59 +1,31 @@
 /**
- * @param {number[][]} arr1
- * @param {number[][]} arr2
- * @returns {number[][]}
- */
-function solution(arr1, arr2) {
-  const result = [];
-  const i = arr1.length;
-  const j = arr2[0].length;
-
-  for (let i = 0; i < arr1.length; i++) {
-    const row = [];
-    for (let j = 0; j < arr2[0].length; j++) {
-      row.push(multiply(extractRow(arr1, i), extractCol(arr2, j)));
-    }
-    result.push(row);
-  }
-
-  return result;
-}
-
-/**
- * @param {number[][]} arr
- * @param {number} idx
- * @returns {number[]}
- */
-function extractRow(arr, idx) {
-  return arr[idx];
-}
-
-/**
- * @param {number[][]} arr
- * @param {number} idx
- * @returns {number[]}
- */
-function extractCol(arr, idx) {
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    result.push(arr[i][idx]);
-  }
-  return result;
-}
-
-/**
- * @param {number[]} arr1
- * @param {number[]} arr2
+ * @param {number} cacheSize
+ * @param {string[]} cities
  * @returns {number}
  */
-function multiply(arr1, arr2) {
-  let sum = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    sum += arr1[i] * arr2[i];
+function solution(cacheSize, cities) {
+  let time = 0;
+  const cache = [];
+  // 도시 탐색
+  for (let i = 0; i < cities.length; i++) {
+    let hitFlag = false;
+    for (let j = 0; j < cache.length; j++) {
+      if (cache[j] === cities[i].toLowerCase()) {
+        time += 1;
+        hitFlag = true;
+        cache.splice(j, 1);
+        cache.unshift(cities[i].toLowerCase());
+        break;
+      }
+    }
+    if (hitFlag) continue;
+
+    // miss
+    time += 5;
+    cache.unshift(cities[i].toLowerCase());
+    if (cache.length > cacheSize) cache.pop();
   }
-  return sum;
+  return time;
 }
 
 export default solution;
-
-export { extractCol, extractRow, multiply };
