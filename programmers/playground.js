656632d3963336962c844a6bbea5dda6fe20800a
arch-spatula <shkim516@naver.com>
2023-09-18 11:39:50 +0900

    1일3제: 의상

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,31 +1,25 @@
 /**
- * @param {number} cacheSize
- * @param {string[]} cities
+ * @param {[string, string][]} clothes
  * @returns {number}
  */
-function solution(cacheSize, cities) {
-  let time = 0;
-  const cache = [];
-  // 도시 탐색
-  for (let i = 0; i < cities.length; i++) {
-    let hitFlag = false;
-    for (let j = 0; j < cache.length; j++) {
-      if (cache[j] === cities[i].toLowerCase()) {
-        time += 1;
-        hitFlag = true;
-        cache.splice(j, 1);
-        cache.unshift(cities[i].toLowerCase());
-        break;
-      }
-    }
-    if (hitFlag) continue;
-
-    // miss
-    time += 5;
-    cache.unshift(cities[i].toLowerCase());
-    if (cache.length > cacheSize) cache.pop();
+function solution(clothes) {
+  let result = 1;
+  const memo = new Map();
+  clothes.forEach(([name, kind]) => {
+    if (!memo.get(kind)) memo.set(kind, [name]);
+    else memo.set(kind, [...memo.get(kind), name]);
+  });
+  // 아래가 어려웠습니다.
+  for (const item of memo.values()) {
+    result *= item.length + 1;
   }
-  return time;
+  // 1종류 1개
+  // nC1 + nC1 ...
+  // 2종류 1개
+  // nC1 * nC1 +
+  // 3종류 1개
+  // 4종류 1개 ...
+  return result - 1;
 }
 
 export default solution;
