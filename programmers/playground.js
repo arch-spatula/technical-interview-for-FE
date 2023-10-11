53055047ad325e2e70f5cf6470549f5955a95a48
arch-spatula <shkim516@naver.com>
2023-10-11 23:17:50 +0900

    1일3제: 피로도 풀이 시도

--- programmers/playground.js
+++ programmers/playground.js
@@ -1,103 +1,25 @@
 /**
- * @param {string} str1
- * @param {string} str2
+ * @param {number} k
+ * @param {[number, number][]} dungeons
  * @returns {number}
  */
-function solution(str1, str2) {
-  // 모두 소문자 처리
-  // 2개 쪼개기
-  const leftList = splitByTwo(str1.toUpperCase());
-  const rightList = splitByTwo(str2.toUpperCase());
-
-  // 교집합
-  const intersectionSet = getIntersection(leftList, rightList);
-
-  // 합집합
-  const unionSet = getUnion(leftList, rightList);
-
-  // 분모 사이즈 0대응하기
-  if (unionSet.length === 0) return 65536;
-  return parseInt((intersectionSet.length / unionSet.length) * 65536);
-}
-
-/**
- * @param {string} str
- * @returns {string[]}
- */
-function splitByTwo(str) {
-  const result = [];
-  for (let i = 0; i < str.length - 1; i += 1) {
-    const item = str.slice(i, i + 2);
-    // 공백이나 숫자, 특수 문자 -> 문나열만 해당하는 정규표현식
-    if (/^[a-zA-Z]+$/.test(item)) result.push(item);
+function solution(k, dungeons) {
+  const sortedDungeons = dungeons.slice().sort((a, b) => {
+    const [entryA, costA] = a;
+    const [entryB, costB] = b;
+    if (entryA / costA > entryB / costB) return 1;
+    else return -1;
+  });
+  let result = 0;
+  const sortedDungeonsLength = sortedDungeons.length;
+  for (let i = 0; i < sortedDungeonsLength; i++) {
+    const currentDungeon = sortedDungeons.pop();
+    const [entry, cost] = currentDungeon;
+    if (entry > k) break;
+    k -= cost;
+    result += 1;
   }
   return result;
 }
 
-/**
- * @param {string[]} set1
- * @param {string[]} set2
- * @returns {string[]}
- */
-function getIntersection(set1, set2) {
-  // 빈도수 카운터
-  /** @type {Map<string, {left: number, right: number}>} */
-  const memo = new Map();
-  set1.forEach((elem) => {
-    const pointer = memo.get(elem);
-    if (pointer)
-      memo.set(elem, { ...pointer, left: pointer.left + 1, right: 0 });
-    else memo.set(elem, { ...pointer, left: 1, right: 0 });
-  });
-  set2.forEach((elem) => {
-    const pointer = memo.get(elem);
-    if (pointer) memo.set(elem, { ...pointer, right: pointer.right + 1 });
-    else memo.set(elem, { ...pointer, right: 1 });
-  });
-  const result = [];
-  memo.forEach((value, key) => {
-    const left = value.left;
-    const right = value.right;
-    if (left > right) result.push(...Array.from({ length: right }, () => key));
-    else result.push(...Array.from({ length: left }, () => key));
-  });
-
-  return result;
-}
-
-/**
- * @param {string[]} set1
- * @param {string[]} set2
- * @returns {string[]}
- */
-function getUnion(set1, set2) {
-  // 빈도수 카운터
-  /** @type {Map<string, {left: number, right: number}>} */
-  const memo = new Map();
-  set1.forEach((elem) => {
-    const pointer = memo.get(elem);
-    if (pointer)
-      memo.set(elem, { ...pointer, left: pointer.left + 1, right: 0 });
-    else memo.set(elem, { ...pointer, left: 1, right: 0 });
-  });
-  set2.forEach((elem) => {
-    let pointer = memo.get(elem);
-    if (!pointer || !pointer.left) memo.set(elem, { ...pointer, left: 0 });
-    pointer = memo.get(elem);
-    if (pointer.right) memo.set(elem, { ...pointer, right: pointer.right + 1 });
-    else memo.set(elem, { ...pointer, right: 1 });
-  });
-
-  const result = [];
-  memo.forEach((value, key) => {
-    const left = value.left;
-    const right = value.right;
-    if (left < right) result.push(...Array.from({ length: right }, () => key));
-    else result.push(...Array.from({ length: left }, () => key));
-  });
-  return result;
-}
-
 export default solution;
-
-export { getUnion, getIntersection, splitByTwo };
--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -1,56 +1,17 @@
 import solution, { getIntersection, getUnion } from './playground';
 import { test, expect, describe } from 'vitest';
 
-// str1	      str2	      answer
-// FRANCE	    french	    16384
-// handshake	shake hands	65536
-// aa1+aa2	  AAAA12	    43690
-// E=M*C^2	  e=m*c^2	    65536
+// k	dungeons	                result
+// 80	[[80,20],[50,40],[30,10]]	3
 
-describe('뉴스 클러스터링', () => {
+describe('피로도', () => {
   test('예제 1', () => {
-    expect(solution('FRANCE', 'french')).toBe(16384);
-  });
-  test('예제 2', () => {
-    expect(solution('handshake', 'shake hands')).toBe(65536);
-  });
-  test('예제 3', () => {
-    expect(solution('aa1+aa2', 'AAAA12')).toBe(43690);
-  });
-  test('예제 4', () => {
-    expect(solution('E=M*C^2', 'e=m*c^2')).toBe(65536);
+    expect(
+      solution(80, [
+        [80, 20],
+        [50, 40],
+        [30, 10],
+      ])
+    ).toBe(3);
   });
 });
-
-describe('helper', () => {
-  test('getUnion - 1', () => {
-    expect(
-      getUnion(['fr', 'ra', 'an', 'nc', 'ce'], ['fr', 're', 'en', 'nc', 'ch'])
-    ).toEqual(['fr', 'ra', 'an', 'nc', 'ce', 're', 'en', 'ch']);
-  });
-  test('getUnion - 2', () => {
-    expect(getUnion(['aa', 'aa'], ['aa', 'aa', 'aa'])).toEqual([
-      'aa',
-      'aa',
-      'aa',
-    ]);
-  });
-  test('getIntersection - 1', () => {
-    expect(
-      getIntersection(
-        ['FR', 'RA', 'AN', 'NC', 'CE'],
-        ['FR', 'RE', 'EN', 'NC', 'CH']
-      )
-    ).toEqual(['FR', 'NC']);
-  });
-  test('getIntersection - 2', () => {
-    expect(getIntersection(['aa', 'aa'], ['aa', 'aa', 'aa'])).toEqual([
-      'aa',
-      'aa',
-    ]);
-  });
-});
-
-// {FR, RA, AN, NC, CE}, {FR, RE, EN, NC, CH}
-// {FR, NC}
-// {FR, RA, AN, NC, CE, RE, EN, CH}
