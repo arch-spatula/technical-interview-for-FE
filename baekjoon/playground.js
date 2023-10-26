18d3bc583d5d1234b2ea46a7ce7525c229ffc0ed
arch-spatula <shkim516@naver.com>
2023-10-26 23:56:52 +0900

    1일3제 달성

--- baekjoon/playground.js
+++ baekjoon/playground.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-const [input] = fs.readFileSync(filePath).toString().split(' ').map(Number);
+const [n, m, k] = fs.readFileSync(filePath).toString().split(' ').map(Number);
 
 /**
- * @param {number} num
+ * @param {number} n
+ * @param {number} m
+ * @param {number} k
  * @returns {number}
  */
-function solution(num) {
-  let result = 1;
-  if (num <= 1) return 1;
-  for (let i = num; 1 <= i; i--) {
-    result *= i;
+function solution(n, m, k) {
+  // 배열 만들기, 원소 탐색
+  let idx = 0;
+
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < m; x++) {
+      if (idx === k) return [y, x];
+      idx += 1;
+    }
   }
-  return result;
+
+  return [n, m];
 }
 
-console.log(solution(input));
+const [x, y] = solution(n, m, k);
+console.log(x, y);
 
 module.exports = {
   solution,
--- baekjoon/playground.test.js
+++ baekjoon/playground.test.js
@@ -3,8 +3,22 @@ import { test, expect, describe } from 'vitest';
 
 describe('N 찍기', () => {
   test('예제 1', () => {
-    expect(solution(0)).toBe(1);
+    expect(solution(3, 4, 6)).toEqual([1, 2]);
+  });
+  test('예제 2', () => {
+    expect(solution(6, 4, 14)).toEqual([3, 2]);
+  });
+  test('예제 3', () => {
+    expect(solution(6, 4, 23)).toEqual([6, 4]);
+  });
+  test('예제 4', () => {
+    expect(solution(6, 4, 23)).toEqual([5, 4]);
   });
 });
 
-// 0 ≤ N ≤ 12
+//  0  1  2  3
+//  4  5  6  7
+//  8  9 10 11
+// 12 13 14 15
+// 16 17 18 19
+// 20 21 22 23
