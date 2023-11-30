c0dccdd669e4b1e9f914d5c76c952bc406f4dcd0
arch-spatula <shkim516@naver.com>
2023-12-01 00:28:26 +0900

    피로도 문제 풀이 달성

--- playground.js
+++ playground.js
@@ -1,24 +1,49 @@
-class HashTable {
-  constructor(...arr) {
-    this.table = [...arr];
-  }
+const graph = {
+  g: ['h', 'i'],
+  h: ['g', 'j'],
+  j: ['h', 'i', 'k', 'l'],
+  i: ['g', 'j'],
+  k: ['j'],
+  l: ['j'],
+};
 
-  add(key, value, idx) {
-    this.table[idx].push([key, value]);
+/**
+ * @param {Record<string, string[]>} graph
+ * @param {string} startingNode
+ * @returns {string[]}
+ */
+function dfs(graph, startingNode) {
+  const visited = new Set();
+  const stack = [startingNode];
+  const result = [];
+  while (stack.length) {
+    const current = stack.pop();
+    if (visited.has(current)) continue;
+    visited.add(current);
+    result.push(current);
+    stack.push(...graph[current]);
   }
+  return result;
 }
 
-const hashTable = new HashTable(
-  [["red", "#FF0000"]],
-  [
-    ["gree", "#00FF00"],
-    ["springgreen", "#00FF7F"],
-  ],
-  [["blue", "#0000FF"]]
-);
+/**
+ * @param {Record<string, string[]>} graph
+ * @param {string} startingNode
+ * @returns {string[]}
+ */
+function bfs(graph, startingNode) {
+  const visited = new Set();
+  const queue = [startingNode];
+  const result = [];
+  while (queue.length) {
+    const current = queue.shift();
+    if (visited.has(current)) continue;
+    visited.add(current);
+    result.push(current);
+    queue.push(...graph[current]);
+  }
+  return result;
+}
 
-console.log(hashTable.table);
-
-hashTable.add("pink", "#FFC0CB", 0);
-
-console.log(hashTable.table);
+console.log(dfs(graph, 'g'));
+console.log(bfs(graph, 'g'));
--- programmers/playground.js
+++ programmers/playground.js
@@ -4,22 +4,37 @@
  * @returns {number}
  */
 function solution(k, dungeons) {
-  const sortedDungeons = dungeons.slice().sort((a, b) => {
-    const [entryA, costA] = a;
-    const [entryB, costB] = b;
-    if (entryA / costA > entryB / costB) return 1;
-    else return -1;
-  });
-  let result = 0;
-  const sortedDungeonsLength = sortedDungeons.length;
-  for (let i = 0; i < sortedDungeonsLength; i++) {
-    const currentDungeon = sortedDungeons.pop();
-    const [entry, cost] = currentDungeon;
-    if (entry > k) break;
-    k -= cost;
-    result += 1;
-  }
-  return result;
+  let result = [];
+  let depth = 0;
+  /**
+   * @param {[number, number][]} visited
+   * @param {[number, number][]} remaining
+   * @param {number} health
+   */
+  (function graph(visited, remaining, health, depth) {
+    // console.log(visited, remaining, health, depth);
+    if (!remaining.length) {
+      result.push(depth);
+      // depth = 0;
+      return;
+    }
+    for (let i = 0; i < remaining.length; i++) {
+      const current = visited.concat(remaining[i]);
+      // 진행여부 가드
+      // 통과 못하면 초기화
+      const [barrier, cost] = remaining[i];
+      if (health < barrier) {
+        graph(current, [], health - cost, depth);
+      } else {
+        const nextRemaining = remaining
+          .slice(i + 1)
+          .concat(remaining.slice(0, i));
+        // depth += 1;
+        graph(current, nextRemaining, health - cost, depth + 1);
+      }
+    }
+  })([], dungeons, k, 0);
+  return Math.max(...result);
 }
 
 export default solution;
