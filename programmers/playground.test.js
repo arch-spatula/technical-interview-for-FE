03f61ac6eb716defa07d4359fd4131119967eecc
arch-spatula <shkim516@naver.com>
2023-12-01 18:00:33 +0900

    advent of code 1일차 2문제 풀이 달성

--- programmers/playground.test.js
+++ programmers/playground.test.js
@@ -4,14 +4,18 @@ import { test, expect, describe } from 'vitest';
 // k	dungeons	                result
 // 80	[[80,20],[50,40],[30,10]]	3
 
-describe('피로도', () => {
+describe('day 1', () => {
   test('예제 1', () => {
     expect(
-      solution(80, [
-        [80, 20],
-        [50, 40],
-        [30, 10],
-      ])
-    ).toBe(3);
+      solution(`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+eightwo
+dsxnfkjn2vtwofivethree2`)
+    ).toBe(385);
   });
 });
